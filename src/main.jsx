c8da7dc31e7b115eac62bc670e14ec7fd5182144
safanasir319-create <safanasir319@gmail.com>
2026-07-30 11:12:27 +0500

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import { PortfolioProvider } from './context/PortfolioContext.jsx'
+import { AuthProvider } from './context/AuthContext.jsx'
 import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <PortfolioProvider>
-        <App />
-      </PortfolioProvider>
+      <AuthProvider>
+        <PortfolioProvider>
+          <App />
+        </PortfolioProvider>
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 )
